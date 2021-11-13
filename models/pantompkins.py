2b1ca5ba6bd691f3bb378bf7a0460cee5d4a6627
Matheus Augusto <matheusamg@gmail.com>
2021-11-13 03:58:09 -0300

    usando funcao filtro

--- models/pantompkins.py
+++ models/pantompkins.py
@@ -1,14 +1,31 @@
 import wfdb
 import numpy as np
+from scipy import signal as sig
+import matplotlib.pyplot as plt
 
 record, fields = wfdb.rdsamp("database_part/s0004_re", channels=[0])
+tamanho = np.linspace(0, len(record), num = len(record))
+
+
 
 def pass_baixa(sinal):
-    lowpass = np.zeros(len(sinal)+1)
-    for i in range(12, len(lowpass)-2):
-        lowpass[i] = 2*lowpass[i+1] - lowpass[i-2] + (1/32*(sinal[i] - 2*sinal[i-6] + sinal[i-12]))
+    lowpass = np.copy(sinal)
+    for i in range(12, len(lowpass)-1):
+        lowpass[i] = (2*lowpass[i-1]) - (lowpass[i-2]) + ((1/32)*(sinal[i] - 2*sinal[i-6] + sinal[i-12]))
     return lowpass 
 
 teste = pass_baixa(record)
 
-print(teste)
+x1 = sig.lfilter([1,0,0,0,0,0,-2,0,0,0,0,0,1],[1,-2,1],record)
+
+# plt.figure(figsize=(10,5))
+# plt.xlabel(xlabel="Time(ms)")
+# plt.ylabel(ylabel="Amplitude")
+# plt.plot(tamanho[0:4000], teste[0:4000])
+# plt.show()
+
+plt.figure(figsize=(10,5))
+plt.xlabel(xlabel="Time(ms)")
+plt.ylabel(ylabel="Amplitude")
+plt.plot(tamanho[0:4000], x1[0:4000])
+plt.show()
