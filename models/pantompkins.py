62ce38caf000b312ef5725641261269664c6b53f
Matheus Augusto <matheusamg@gmail.com>
2021-11-13 05:32:01 -0300

    confirmado

--- models/pantompkins.py
+++ models/pantompkins.py
@@ -7,7 +7,6 @@ record, fields = wfdb.rdsamp("database_part/s0004_re", channels=[0])
 tamanho = np.linspace(0, len(record), num = len(record))
 
 
-
 def pass_baixa(sinal):
     lowpass = np.copy(sinal)
     for i in range(12, len(lowpass)-1):
@@ -16,16 +15,9 @@ def pass_baixa(sinal):
 
 teste = pass_baixa(record)
 
-x1 = sig.lfilter([1,0,0,0,0,0,-2,0,0,0,0,0,1],[1,-2,1],record)
-
-# plt.figure(figsize=(10,5))
-# plt.xlabel(xlabel="Time(ms)")
-# plt.ylabel(ylabel="Amplitude")
-# plt.plot(tamanho[0:4000], teste[0:4000])
-# plt.show()
 
 plt.figure(figsize=(10,5))
 plt.xlabel(xlabel="Time(ms)")
 plt.ylabel(ylabel="Amplitude")
-plt.plot(tamanho[0:4000], x1[0:4000])
+plt.plot(tamanho[0:4000], teste[0:4000])
 plt.show()
