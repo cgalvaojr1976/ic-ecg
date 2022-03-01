2b2cdf458cff4279061e13121806cb5c583dce60
Matheus Augusto <matheusamg@gmail.com>
2022-03-01 12:33:56 -0300

    montagem do exploratory

--- datasets/batimentos.py
+++ datasets/batimentos.py
@@ -9,37 +9,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 import wfdb
 
-# diretorio = '/home/matheus/Documentos/Facul/IC/ECG_classification/data/'
+diretorio = '/home/matheus/Documentos/Facul/IC/ECG_classification/data/'
 
 # Lista com os códigos que precisamos
-# codigo = ['10370003', '164947007'] # PR e LPR
+codigo = ['426627000']
 
 # Lista dos leads
-# leads = [0,1,2]
+leads = [0,1,2]
 
 def records(diretorio, codigo, leads):
     # Criar tabela que contenha os Datasets e os Arquivos com os códigos
-    data_frame = pd.read_csv('Total.csv')
-
+    data_frame = pd.read_csv('/home/matheus/Documentos/Facul/IC/ECG_classification/ic-ecg/datasets/Total.csv')
+    print(data_frame)
     # Cria tabela com apenas os códigos necessários
     df = pd.DataFrame(columns= ['arquivos', 'base', 'Dx'])
-    for x in range(len(codigo)):
-        df = df.append(data_frame.loc[lambda data_frame: data_frame['Dx'] == codigo[x]])
+    df = df.append(data_frame.loc[lambda data_frame: data_frame['Dx'] == codigo[x]])
 
     df = df.reset_index()
+    print(df)
     #Cria a tabela dos records
     records = pd.DataFrame()
     # Leitura dos records
     for x in range(len(df)):
         destino = diretorio + df.loc[x]['base'] + '/' + df.loc[x]['arquivos']
-        record, fields = wfdb.rdsamp(destino, channels = leads)
-        # Inserir linha de leads
-        record = np.insert(record, 0 , leads, axis= 0)
-        # Inserir lina de código de arritmia
-        record = np.insert(record, 1, [df.loc[x]['Dx'] for x in range(len(leads))], axis= 0)
+        print(destino)
+
+        if df.loc[x]['fs'] == 500.0:
+            record, fields = wfdb.rdsamp(destino, channels = leads, sampto= 5000)
+        elif df.loc[x]['fs'] == 1000.0:
+            record, fields = wfdb.rdsamp(destino, channels = leads, sampto= 5000)
+        elif df.loc[x]['fs'] == 257:
+            record, fields = wfdb.rdsamp(destino, channels = leads, sampto= 5000)
+
         # Colocar o record em uma data_frame Junto com seu código
         aux = pd.DataFrame(record.transpose())
+        # Inserir a linha com o nome da arritmia
+        aux['arritmia'] = int(df.loc[x]['Dx'])
+        # Inserir coluna de leads
+        aux['leads'] = leads
+        # Inserir a coluna com nome do arquivo
+        aux['arquivo'] = df.loc[x]['arquivos']
         records = records.append(aux)
 
     # Gravar Tabela
-    records.to_csv('records.csv')
+    records.to_csv('records1.csv')
+
+records(diretorio,codigo,leads)
